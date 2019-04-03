5e3105a66692b3c22d17ff1b312857d095917210
Akanksha Shukla <akanksha.v.shukla@gmail.com>
2019-04-02 22:03:22 -0500

    Create ReverseInteger.java

---
+++ ReverseInteger.java
@@ -0,0 +1,11 @@
+class ReverseInteger {
+    public int reverse(int x) {
+        int res=0;
+        while(Math.abs(x)>0){
+            if(res*10/10 != res) return 0;
+            res=(res*10) + (x%10);
+            x=x/10;
+        }
+        return  res;
+    }
+}
